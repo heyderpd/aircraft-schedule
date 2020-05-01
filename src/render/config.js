3db9b70af2c2e2b9ad4f3c3758c5b0949740527a
heyder.dias <heyder.dias@zoop.com.br>
2020-05-01 03:16:41 -0300

    create config

---
+++ src/render/config.js
@@ -0,0 +1,11 @@
+export const page_y_limit = 1000
+
+export const page_x_limit = 380
+
+export const box_width = 200
+
+export const box_height = 80
+
+export const box_marge = 3
+
+export const line_height = 59

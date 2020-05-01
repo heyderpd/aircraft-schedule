0fee2f0c44d3b3298707240ab307ba3a2d548732
heyder.dias <heyder.dias@zoop.com.br>
2020-04-30 23:39:47 -0300

    fix pkg

---
+++ src/render/index.js
@@ -0,0 +1,4 @@
+import { on_ready } from './utils'
+import main from './main'
+
+on_ready(main)
---
+++ src/render/utils.js
@@ -0,0 +1,11 @@
+export const on_ready = load => {
+    const on_ready = () => setTimeout(
+      load,
+      200,
+    )
+
+    document.readyState !== 'complete'
+      ? document.addEventListener('DOMContentLoaded', load)
+      : on_ready()
+  }
+}

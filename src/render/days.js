0d0d4a37f145a6815ec032d73192396809362403
heyder.dias <heyder.dias@zoop.com.br>
2020-05-01 03:40:42 -0300

    add hours

--- src/render/days.js
+++ src/render/days.js
@@ -2,26 +2,47 @@ import { page_x_limit, box_width, box_height } from './config'
 import { range } from './utils'
 import { getNow } from './time'
 
-const box = offset => new Konva.Rect({
-  x: (offset * box_width),
-  y: 0,
-  width: box_width,
-  height: box_height,
-  fill: '#e2e2e2',
-  stroke: 'black',
-  strokeWidth: 1,
-  draggable: false,
-})
+const box = layer => offset => {
+  layer.add(new Konva.Rect({
+    x: (offset * box_width),
+    y: 0,
+    width: box_width,
+    height: box_height,
+    fill: '#e2e2e2',
+    stroke: 'black',
+    strokeWidth: 1,
+    draggable: false,
+  }))
 
-const text = (offset, text, move_width=0) => {
+  const elms = [0, 6, 12, 18]
+    .map((_t, _po) => {
+      const off = (box_width /4 *_po) - box_width /2
+      console.log(77, { offset, off })
+      return text(offset, _t, 12, off, 35)
+    })
+  elms.map(elm => layer.add(elm))
+}
+
+const getDay = now => {
+  if (now === null) {
+    return -1
+  }
+  return (now -(now %box_width)) /box_width
+}
+
+const text = (offset, text, size=20, move_x=0, move_y=0, bold=false, now=null) => {
+  const off = getDay(now)
+  console.log({ offset, off })
+  const color = offset == off ? 'white' : 'black'
   const elm = new Konva.Text({
-    x: (box_width /2) + (offset *box_width),
-    y: box_height/2 +move_width,
+    x: (box_width /2) +(offset *box_width) +move_x,
+    y: box_height/2 +move_y,
     text: text,
-    fontSize: 25,
+    fontSize: size,
     fontFamily: 'Calibri',
-    fill: 'black',
-    align: 'center'
+    fill: color,
+    align: 'center',
+    fontStyle: bold ? 'bold' : '',
   })
   elm.offsetX(elm.width() /2)
   elm.offsetY(elm.height() /2)
@@ -29,13 +50,12 @@ const text = (offset, text, move_width=0) => {
 }
 
 const circle = now => {
-  const offset = (now -(now %box_width)) /box_width
+  const offset = getDay(now)
   return new Konva.Circle({
     x: (box_width /2) + (offset *box_width),
-    y: box_height/2 +10,
+    y: box_height/2 +12,
     radius: box_height /6,
-    stroke: 'gray',
-    strokeWidth: 4,
+    fill: 'black',
   })
 }
 
@@ -56,14 +76,13 @@ const days = (layer, data) => {
     .map(line)
     .map(elm => layer.add(elm))
   range(5)
-    .map(box)
-    .map(elm => layer.add(elm))
+    .map(box(layer))
   layer.add(circle(now))
   range(5)
-    .map(offset => text(offset, day[offset], -10))
+    .map(offset => text(offset, day[offset], 20, 0, -15))
     .map(elm => layer.add(elm))
   range(5)
-    .map(offset => text(offset, num[offset], 10))
+    .map(offset => text(offset, num[offset], 20, 0, 15, true, now))
     .map(elm => layer.add(elm))
 }
 
